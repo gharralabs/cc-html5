cbc309ab79c5741a2a60253f1ae9765807f97b5c
Marine King <mkp@prime.co.kr>
2015-10-11 00:24:46 -0300

    Spritesheet

---
+++ html5/05-spritesheet.js
@@ -0,0 +1,43 @@
+﻿function Spritesheet(context, imagem, linhas, colunas) {
+    this.context = context;
+    this.imagem = imagem;
+    this.numLinhas = linhas;
+    this.numColunas = colunas;
+    this.intervalo = 0;
+    this.linha = 0;
+    this.coluna = 0;
+}
+
+
+Spritesheet.prototype = {
+    proximoQuadro: function () {
+
+        var agora = new Date().getTime();
+
+        if (!this.ultimoTempo)
+            this.ultimoTempo = agora;
+
+        if (agora - this.ultimoTempo < this.intervalo)
+            return;
+
+        if (this.coluna < this.numColunas - 1)
+            this.coluna++;
+        else
+            this.coluna = 0;
+
+
+        this.ultimoTempo = agora;
+    },
+
+    desenhar: function (x, y) {
+        var larguraQuadro = this.imagem.width / this.numColunas;
+        var alturaQuadro = this.imagem.height / this.numLinhas;
+
+
+        this.context.drawImage(this.imagem,
+            larguraQuadro * this.coluna, alturaQuadro * this.linha,
+            larguraQuadro, alturaQuadro,
+            x, y,
+            larguraQuadro, alturaQuadro);
+    }
+}
