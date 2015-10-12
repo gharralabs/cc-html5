6124bda49c0d8e6d3f39c87926f9caa318f2613f
Marine King <mkp@prime.co.kr>
2015-10-12 00:47:21 -0300

    Colisor força bruta

---
+++ html5/06-colisor.js
@@ -0,0 +1,48 @@
+﻿function Colisor() {
+    this.sprites = [];
+}
+
+Colisor.prototype = {
+
+    novoSprite: function (sprite) {
+        this.sprites.push(sprite);
+    },
+
+    processar: function () {
+        for( var i in this.sprites ){
+            for (var j in this.sprites) {
+
+                if (i == j)
+                    continue;
+
+                this.testarColisao(this.sprites[i], this.sprites[j]);
+            }
+        }
+    },
+
+    testarColisao: function (sprite1, sprite2) {
+
+        var rets1 = sprite1.retangulosColisao();
+        var rets2 = sprite2.retangulosColisao();
+
+        for (var i in rets1) {
+            for (var j in rets2) {
+                if (this.retangulosColidem(rets1[i], rets2[j])) {
+                    sprite1.colidiuCom(sprite2);
+                    sprite2.colidiuCom(sprite1);
+                    break;
+                }
+            }
+        }
+    },
+
+    retangulosColidem: function (ret1, ret2) {
+
+        return (ret1.x + ret1.largura) > ret2.x && 
+                ret1.x < ( ret2.x + ret2.largura) && 
+               (ret1.y + ret1.altura) > ret2.y &&
+                ret1.y < ( ret2.y + ret2.altura);
+    }
+
+
+}
