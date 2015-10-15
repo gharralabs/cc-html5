4a4c138e55cb3f6e19f9c8be002e9213b41a565e
Marine King <mkp@prime.co.kr>
2015-10-15 12:40:43 -0300

    09 - painel

---
+++ html5/09-painel.js
@@ -0,0 +1,38 @@
+﻿function Painel(context, nave) {
+
+    this.context = context;
+    this.nave = nave;
+    this.spritesheet = new Spritesheet(context, nave.imagem, 3, 2);
+    this.spritesheet.linha = 0;
+    this.spritesheet.coluna = 0;
+
+    this.pontuacao = 0;
+}
+
+
+Painel.prototype = {
+    atualizar: function () {
+
+    },
+    
+    desenhar: function () {
+
+        var ctx = this.context;
+        ctx.save();
+        ctx.scale(0.5, 0.5);
+        var x = 20;
+        var y = 20;
+
+        for (var i = 1; i <= this.nave.vidas; i++) {
+            this.spritesheet.desenhar(x, y);
+            x += 40;
+        }
+        ctx.restore();
+
+        ctx.save();
+        ctx.fillStyle = "white";
+        ctx.font = '18px sans-serif';
+        ctx.fillText(this.pontuacao, 100, 27);
+        ctx.restore();
+    }
+}
